5fdd9b83542d6626f8b0a138908905a4f243512f
dulajkavinda <dulajkaavinda@gmail.com>
2021-09-18 19:09:29 +0530

    finished

--- src/app.ts
+++ src/app.ts
@@ -26,14 +26,30 @@ io.on("connection", (socket: Socket) => {
 
 namespaces.forEach((namespace) => {
   io.of(namespace.endpoint).on("connection", (nsSocket: Socket) => {
-    console.log(`${nsSocket.id} has joined ${namespace.endpoint}`);
+    nsSocket.emit("nsRoomLoad", namespace.rooms);
+    nsSocket.on("joinRoom", async (roomToJoin, newNoOfMembers) => {
+      console.log(nsSocket.rooms);
 
-    if (namespace.endpoint === "/wiki") {
-      nsSocket.emit("nsRoomLoad", namespaces[0].rooms);
-    } else if (namespace.endpoint === "/mozilla") {
-      nsSocket.emit("nsRoomLoad", namespaces[1].rooms);
-    } else {
-      nsSocket.emit("nsRoomLoad", namespaces[2].rooms);
-    }
+      nsSocket.join(roomToJoin);
+      const ids = await io.of(namespace.endpoint).in(roomToJoin).allSockets();
+
+      const nsRoom = namespace.rooms.find((room) => {
+        return room.roomTitle === roomToJoin;
+      });
+
+      nsSocket.emit("history", nsRoom.history);
+      io.of(namespace.endpoint).in(roomToJoin).emit("updateMembers", ids.size);
+    });
+    nsSocket.on("newMessageToServer", (msg) => {
+      const nsRoom = namespaces[0].rooms.find((room) => {
+        return room.roomTitle === msg.roomName;
+      });
+
+      nsRoom.addmessage(msg.message);
+
+      io.of(namespace.endpoint)
+        .to(msg.roomName)
+        .emit("messageToClient", msg.message);
+    });
   });
 });
